a966840897c2ed2a65841b44ee59664dd7b22239
MrVermilion <behelton3@gmail.com>
2024-08-21 14:25:29 -0300

    commit

--- api/index.php
+++ api/index.php
@@ -1,9 +1,10 @@
 <?php
 
-header('content-type: application/json');
-echo json_encode([
-    'time' => time(),
-    'date' => date('d.m.Y'),
-    'tech' => 'Vercel'
-]);
+require_once("core/header.php");
 
+echo '<link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="css/button.css">';
+
+require_once("core/sistema.php");
+
+require_once("core/footer.php");
